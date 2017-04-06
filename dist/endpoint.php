d63823a30cf1ae26cb4677f63abec7e72b12210c
Jesse Clark <contact@jessclark.com>
2017-04-06 15:52:34 -0700

    temp

--- dist/endpoint.php
+++ dist/endpoint.php
@@ -11,16 +11,43 @@
 
 	// authenticate and create $user:
 	$a = new authenticate();
-	$user = $a->login("alexa", "password");
+	$user = $a->login("alexa", "alexa");
 
-
-	// task:
-	$t = new checkInFiles($user);
-
-	//var_dump($t);
 
 	// send JSON response to Alexa:
 	$r = new response();
-	$r->returnText($t->getResponse());	
+
+	// get intent
+	// refernce: https://gist.github.com/solariz/a7b7b09e46303223523bba2b66b9b341
+	$rawJSON = file_get_contents('php://input');
+	$EchoReqObj = json_decode($rawJSON);
+
+	$intentType;
+	if (is_object($EchoReqObj) === false){
+		$intentType = "CheckInFiles";
+	}else{
+		$intentType = $EchoReqObj->request->intent->name;
+	}
+
+	// intent
+	if ($intentType="CheckInFiles"){
+		$t = new checkInFiles($user);
+		$r->returnText($t->getResponse());
+	}else{
+		$r->returnText("Sorry, I didn't understand the intent ''.");
+	}
+	//var_dump($t);
+
+
+	//var_dump($_REQUEST);
+
+	//$rawJSON = file_get_contents('php://input');
+	//$EchoReqObj = json_decode($rawJSON);
+
+	//$r->returnText( $_SERVER['REQUEST_METHOD'] );	// returns POST
+	//$r->returnText( $_SERVER['REQUEST_URI'] );		// returns "/hack/endpoint.php"
+	//$r->returnText($intentType);
+
+	//);	
 
 ?>
